3657b28df0d2c8b46c18c311c01e9260af8bfb08
Fervil <fervil.darwin@gmail.com>
2019-04-30 17:15:49 +0200

    io

--- io/io.js
+++ io/io.js
@@ -1,5 +1,14 @@
+const fs = require('fs');
 module.exports.decodeHexFileContent = (filePath) =>  {
     return new Promise((resolve, reject) => {
+        var content = fs.readFileSync(filePath, 'utf8');
+        content = Buffer.from(content, 'hex').toString('utf8');
+        if(content)
+            resolve(content);
+        else{
+            reject();
+        }
 
     });
-}
+
+};
